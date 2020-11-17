826fcb689bbd75801741ac69f0b1c161580ed96c
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-11-17 18:52:27 -0500

    Done.

---
+++ testing.js
@@ -0,0 +1,37 @@
+posts = [
+    {
+        title: "Watercolor in Italy",
+        image: "https://i.pinimg.com/originals/67/cc/8d/67cc8d0b868bc7bd87e4373f873fcf94.jpg",
+        description: "I have a passion for watercolors. Italy is one of my favorite places in the world. Last time I was in Venice I was inspired by the colors and energy and this is one of the watercolors I made!",
+        category: "paintings",
+        user_id: 1
+    },
+    {
+        title: "Oil Portaits",
+        image: "https://cdn.thecoolist.com/wp-content/uploads/2012/09/oil-painting-portraits-by-artist-harding-meyer-4.jpg",
+        description: "I love working with oil paints and focusing on peoples faces.",
+        category: "paintings",
+        user_id: 2
+    },
+    {
+        title: "MTA Series: drawing beautiful is easy",
+        image: "https://im.vsco.co/aws-us-west-2/f6fce8/110342396/5c8e83d56c447110232a4761/vsco5c8e83d6bbd80.jpg?w=600&c=1&dpr=2",
+        description: "I am a NYC native and I love capturing beautiful moments in the NYC subway.",
+        category: "photography",
+        user_id: 3
+    },
+    {
+        title: "SSSSculpture",
+        image: "https://i.ytimg.com/vi/pXbDsAE6lko/maxresdefault.jpg",
+        description: "Check out the first snake clay sculpture I made! I love sculpting and can't wait to create more",
+        category: "random",
+        user_id: 1
+    },
+    {
+        title: "BEEEE BUZZ",
+        image: "https://ranartblog.com/imagespencil/bee-pencil-drawing.jpg",
+        description: "This bee drawing was done all in pencil. Improving my shading and details every day!",
+        category: "drawings",
+        user_id: 2
+    }
+]
